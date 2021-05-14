cdac0ebbd600771f73757a0a42e08773fcdca2ea
tjorge30 <mrs.javalera@gmail.com>
2021-05-14 16:08:42 -0600

    app is now full crud

--- server/controllers/commentsCtrl.js
+++ server/controllers/commentsCtrl.js
@@ -1,28 +1,18 @@
-const comments = [
-    {
-        id: 0,
-        comment: "Beautiful paintings thanx for sharing! - Tamra"
-    },
-    {
-        id: 1,
-        comment: "Stunning! - Frank"
-    }
-];
 
-let id = comments.length;
+const comments = require('../../comments.json')
+
 
 module.exports = {
     getComments: (req, res) =>{
-        console.log(comments)
         res.status(200).send(comments)
     },
     postComment: (req, res) =>{
-        console.log(req.body);
         var newComment = {
             id: comments.length + 1,
-            comment: req.body
+            comment: req.body.comment
         };
+    
         comments.push(newComment);
-        res.sendStatus(200)
+        res.status(200).send(comments)
     }
 }
